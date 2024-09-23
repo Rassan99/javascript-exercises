25b595ab43390572541421b27e3a8287795a5f37
Rassan99 <99618618+Rassan99@users.noreply.github.com>
2024-09-23 15:49:22 +0300

    Feat: reverseString is done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,15 +1,14 @@
 const reverseString = function(word) {
     const arrayWord = Array.from(word);
-    console.log(arrayWord);
+    
     let string =[];
     let i = arrayWord.length;
     for(i; i>0 ; i--){
-        console.log(i);
+        
         string.push(arrayWord[i-1]);
     }
     string = string.join('')
     return string;
 };
-console.log( reverseString('hi my name !')  ) ;
 // Do not edit below this line
 module.exports = reverseString;
