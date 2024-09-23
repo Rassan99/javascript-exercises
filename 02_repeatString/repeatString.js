d957d28b6df9d81d6def5c02585c5004d7d248f4
Rassan99 <99618618+Rassan99@users.noreply.github.com>
2024-09-23 14:57:52 +0300

    Fix:Passed the repeatString test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(text,times) {
+    
+   if (times<0){
+    return text = 'ERROR';
+   }
+   let string ='';
+   for (let i=0 ; i<times ; i++){
+      string += text;
+   }
+   return string;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
